920fc86823fb9e2cf4d1635771f0b6640f8e3087
rezazrna <rezazesariana58@gmail.com>
2022-09-29 15:59:53 +0700

    laba rugi ditahan di TB 2

--- vet_website/vet_website/doctype/vetcoa/vetcoa.py
+++ vet_website/vet_website/doctype/vetcoa/vetcoa.py
@@ -432,11 +432,11 @@ def get_coa_last_total(coa_name, max_date=False, no_min_date=False, mode=False):
 	if len(ji_list) != 0:
 		if coa.account_type in ['Asset','Expense']:
 			for ji in ji_list:
-				if (('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name '2-' in coa.name '3-' in coa.name):
+				if (('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name or '2-' in coa.name or '3-' in coa.name):
 					total += ji.debit - ji.credit
 		elif coa.account_type in ['Equity','Income','Liability']:
 			for ji in ji_list:
-				if (('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name '2-' in coa.name '3-' in coa.name):
+				if (('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name or '2-' in coa.name or '3-' in coa.name):
 					total += ji.credit - ji.debit
 		# total = total + ji_list[0].total
 		
@@ -548,10 +548,10 @@ def get_coa_total_debit_credit(name, journal_items=False, journal_items_if_empty
 	journal_item = False
 	
 	if journal_items:
-		journal_item = next(filter(lambda item: item.account == name and ((('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name '2-' in coa.name '3-' in coa.name)), journal_items), None)
+		journal_item = next(filter(lambda item: item.account == name and ((('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name or '2-' in coa.name or '3-' in coa.name)), journal_items), None)
 
 	if not journal_item:
-		journal_item = next(filter(lambda item: item.account == name and ((('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name '2-' in coa.name '3-' in coa.name)), journal_items_if_empty), None)
+		journal_item = next(filter(lambda item: item.account == name and ((('4-' in coa.name or '5-' in coa.name or '6-' in coa.name or '7-' in coa.name or '8-' in coa.name) and ji.journal != 'CLS') or ('1-' in coa.name or '2-' in coa.name or '3-' in coa.name)), journal_items_if_empty), None)
 
 	if journal_item:
 		if coa.account_type in ['Asset','Expense']:
