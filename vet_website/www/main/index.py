aaa194a48b9bda7440529f608f40ff13694f783d
rezazrna <rezazesariana58@gmail.com>
2022-12-15 06:43:08 +0700

    coba /login 2

--- vet_website/www/main/index.py
+++ vet_website/www/main/index.py
@@ -3,7 +3,7 @@ import frappe
 def get_context(context):
 	context.no_cache = True
 	if frappe.session.user == 'Guest':
-		frappe.local.flags.redirect_location = '/login'
+		frappe.local.flags.redirect_location = frappe.request.url + 'login'
 		raise frappe.Redirect
 
 	return context
