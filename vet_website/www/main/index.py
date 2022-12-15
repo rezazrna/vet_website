b49f3d69dd079fbc9d6e952e5bc48e25cc1fcb52
rezazrna <rezazesariana58@gmail.com>
2022-12-15 10:48:24 +0700

    fix port /login

--- vet_website/www/main/index.py
+++ vet_website/www/main/index.py
@@ -3,7 +3,7 @@ import frappe
 def get_context(context):
 	context.no_cache = True
 	if frappe.session.user == 'Guest':
-		frappe.local.flags.redirect_location = frappe.request.url + 'login'
+		frappe.local.flags.redirect_location = frappe.utils.get_url('/login')
 		raise frappe.Redirect
 
 	return context
