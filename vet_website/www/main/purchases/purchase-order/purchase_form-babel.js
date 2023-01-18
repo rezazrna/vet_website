39fbe5c720ad73e5b198e26c5147e8ff35d0a8ea
rezazrna <rezazesariana58@gmail.com>
2023-01-18 18:02:05 +0700

    refund purchase 3

--- vet_website/www/main/purchases/purchase-order/purchase_form-babel.js
+++ vet_website/www/main/purchases/purchase-order/purchase_form-babel.js
@@ -989,6 +989,14 @@ class PopupRefund extends React.Component {
             });
         }
     }
+
+    reverseFormatNumber(val,locale){
+        var group = new Intl.NumberFormat(locale).format(1111).replace(/1/g, '');
+        var decimal = new Intl.NumberFormat(locale).format(1.1).replace(/1/g, '');
+        var reversedVal = val.replace(new RegExp('\\' + group, 'g'), '');
+        reversedVal = reversedVal.replace(new RegExp('\\' + decimal, 'g'), '.');
+        return Number.isNaN(reversedVal) ? '' : reversedVal;
+    }
     
     render() {
         var th = this
