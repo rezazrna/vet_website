3cba7a74828155ffc8e6479faf9d90d687bc6c53
rezazrna <rezazesariana58@gmail.com>
2023-02-07 14:53:29 +0700

    format column text 4

--- vet_website/www/main/accounting/journal-items/index-babel.js
+++ vet_website/www/main/accounting/journal-items/index-babel.js
@@ -471,6 +471,10 @@ class JournalItems extends React.Component {
                 this.formatColumn(sheet, col, format)
             }
 
+            for (let col of [3]) {
+                this.formatColumn(sheet, col, '@')
+            }
+
             for (let col of [0]) {
                 this.formatColumn(sheet, col, 'dd-mm-yyyy')
             }
@@ -596,7 +600,8 @@ class JournalItems extends React.Component {
         // note: range.s.r + 1 skips the header row
         for (let row = range.s.r + 1; row <= range.e.r; ++row) {
             const ref = XLSX.utils.encode_cell({ r: row, c: col })
-            if (worksheet[ref] && worksheet[ref].t === 'n') {
+            // if (worksheet[ref] && worksheet[ref].t === 'n') {
+            if (worksheet[ref]) {
                 worksheet[ref].z = fmt
             }
         }
