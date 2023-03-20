395c6dd344828e902fbc4c05339a287303a6a707
rezazrna <rezazesariana58@gmail.com>
2023-03-20 16:48:57 +0700

    Detail Penjualan 6

--- vet_website/www/main/accounting/detail-penjualan/index-babel.js
+++ vet_website/www/main/accounting/detail-penjualan/index-babel.js
@@ -300,6 +300,7 @@ class DetailPenjualanListRow extends React.Component {
     render() {
         var row_style = {color: '#056EAD', background: '#84D1FF', 'boxShadow': '0px 6px 23px rgba(0, 0, 0, 0.1)', borderRadius: '5px'}
         var detail_style = {background: '#F5FBFF', padding: '20px 0'}
+        var padding_style = {padding: '10px 0', border: '1px solid #397DA6'}
         var color = {color: '#056EAD'}
         var item = this.props.item
         var line_group
@@ -339,7 +340,7 @@ class DetailPenjualanListRow extends React.Component {
                     </div>
                 </div>
                 {rows}
-                <div className="row mx-0 fs14 fw600">
+                <div className="row mx-0 fs14 fw600" style={padding_style}>
                     <div className="col d-flex"></div>
                     <div className="col d-flex"></div>
                     <div className="col d-flex">
@@ -352,6 +353,27 @@ class DetailPenjualanListRow extends React.Component {
                     <div className="col d-flex"></div>
                     <div className="col d-flex">
                         <span className="my-auto">{formatter.format(item.subtotal)}</span>
+                    </div>
+                </div>
+                <div className="row mx-0 fs14 fw600">
+                    <div className="col d-flex">
+                        <span className="my-auto">Pot   :</span>
+                    </div>
+                    <div className="col d-flex">
+                        <span className="my-auto">{item.potongan}</span>
+                    </div>
+                    <div className="col d-flex">
+                        <span className="my-auto">Pajak :</span>
+                    </div>
+                    <div className="col d-flex">
+                        <span className="my-auto">0</span>
+                    </div>
+                    <div className="col d-flex"></div>
+                    <div className="col d-flex">
+                        <span className="my-auto">Total Akhir   :</span>
+                    </div>
+                    <div className="col d-flex">
+                        <span className="my-auto">{formatter.format(item.total)}</span>
                     </div>
                 </div>
             </div>
@@ -377,27 +399,6 @@ class DetailPenjualanListRow extends React.Component {
                     <div className="col d-flex"></div>
     			</div>
     			{line_group}
-                <div className="row mx-0 fs14 fw600">
-                    <div className="col d-flex">
-                        <span className="my-auto">Pot   :</span>
-                    </div>
-                    <div className="col d-flex">
-                        <span className="my-auto">{item.potongan}</span>
-                    </div>
-                    <div className="col d-flex">
-                        <span className="my-auto">Pajak :</span>
-                    </div>
-                    <div className="col d-flex">
-                        <span className="my-auto">0</span>
-                    </div>
-                    <div className="col d-flex"></div>
-                    <div className="col d-flex">
-                        <span className="my-auto">Total Akhir   :</span>
-                    </div>
-                    <div className="col d-flex">
-                        <span className="my-auto">{formatter.format(item.total)}</span>
-                    </div>
-                </div>
 			</div>
         )
     }
