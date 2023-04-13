74a74be0254085eb96fece2d22cd390dce50e306
Reza Zesariana <rezazesariana58@gmail.com>
2023-04-13 09:29:19 +0700

    mengembalikan filter mutasi hutang-piutang

--- vet_website/www/main/kasir/mutasi-piutang/index-babel.js
+++ vet_website/www/main/kasir/mutasi-piutang/index-babel.js
@@ -7,15 +7,60 @@ class MutasiPiutang extends React.Component {
             'currentpage': 1,
             'search': false,
             'datalength': 0,
+            'month': '',
+            'year': '',
             'print_loading': false,
             'print_data': [],
+            'list_year': []
         }
 
+        this.setFilter = this.setFilter.bind(this);
         this.paginationClick = this.paginationClick.bind(this);
     }
 
     componentDidMount() {
+        var td = this
+
+        frappe.call({
+            type: "GET",
+            method: "vet_website.methods.get_list_year",
+            callback: function (r) {
+                if (r.message) {
+                    console.log(r.message);
+                    td.setState({ 'list_year': r.message });
+                }
+            }
+        });
+
         sessionStorage.setItem(window.location.pathname, JSON.stringify({ filters: [], sorts: [] }))
+    }
+
+    filterChange(e) {
+        var th = this
+        var name = e.target.name
+        var value = e.target.value
+        if (name == 'month') {
+            var invoice_date
+            this.setState({ month: value })
+            invoice_date = moment(this.state.year + '-' + value, 'YYYY-MM').add(1, 'month').format('YYYY-MM-DD')
+            th.setState({ invoice_date: invoice_date })
+        } else if (name == 'year') {
+            var invoice_date
+            this.setState({ year: value })
+            if (this.state.mode == 'annual') {
+                invoice_date = moment(value + '-12-31', 'YYYY-MM-DD').format('YYYY-MM-DD')
+            } else {
+                invoice_date = moment(value + '-' + this.state.month, 'YYYY-MM').add(1, 'month').format('YYYY-MM-DD')
+            }
+
+            th.setState({ invoice_date: invoice_date })
+        }
+    }
+
+    setMode(e) {
+        var th = this
+        var mode = e.target.value
+        th.setState({ 'mode': mode, 'month': '', 'year': '' })
     }
 
     paginationClick(number) {
@@ -34,7 +79,7 @@ class MutasiPiutang extends React.Component {
         frappe.call({
             type: "GET",
             method: "vet_website.vet_website.doctype.vetcustomerinvoice.vetcustomerinvoice.get_mutasi_piutang",
-            args: { filters: filters },
+            args: { filters: filters, mode: td.state.mode, },
             callback: function (r) {
                 if (r.message) {
                     console.log(r.message)
@@ -44,30 +89,81 @@ class MutasiPiutang extends React.Component {
         });
     }
 
+    setFilter(filters = false) {
+        var td = this
+        if (!filters) {
+            filters = JSON.parse(sessionStorage.getItem(window.location.pathname))
+        }
+        console.log(this.state.mode)
+        console.log(this.state.month)
+        console.log(this.state.year)
+        console.log(this.state.invoice_date)
+        console.log(this.state.gudang)
+        if ((((this.state.mode == 'monthly' || this.state.mode == 'period') && this.state.month != '') || (this.state.mode == 'annual')) && this.state.year != '') {
+            td.setState({ 'loaded': false, 'currentpage': 1 })
+            filters['currentpage'] = 1
+            filters['search'] = this.state.search
+            filters['invoice_date'] = this.state.invoice_date
+            sessionStorage.setItem(window.location.pathname, JSON.stringify(filters))
+            console.log(filters)
+            frappe.call({
+                type: "GET",
+                method: "vet_website.vet_website.doctype.vetcustomerinvoice.vetcustomerinvoice.get_mutasi_piutang",
+                args: { filters: filters, mode: td.state.mode, },
+                callback: function (r) {
+                    if (r.message) {
+                        console.log(r.message)
+                        td.setState({ 'data': r.message.data, 'loaded': true, 'datalength': r.message.datalength });
+                    }
+                }
+            });
+        } else {
+            frappe.msgprint(('Month or Year must be selected'));
+        }
+    }
+
     getPrintData() {
         var td = this
 
         if (!this.state.print_loading) {
             this.setState({ print_loading: true })
-            frappe.call({
-                type: "GET",
-                method: "vet_website.vet_website.doctype.vetcustomerinvoice.vetcustomerinvoice.get_mutasi_piutang",
-                args: { filters: JSON.parse(sessionStorage.getItem(window.location.pathname)), all: 1 },
-                callback: function (r) {
-                    if (r.message) {
-                        console.log(r.message)
-                        td.setState({ 'print_data': r.message.data, 'loaded': true, 'datalength': r.message.datalength });
-                        setTimeout(function() {
-                            td.printPDF()
-                        }, 3000);
+            if ((((this.state.mode == 'monthly' || this.state.mode == 'period') && this.state.month != '') || (this.state.mode == 'annual')) && this.state.year != '') {
+                frappe.call({
+                    type: "GET",
+                    method: "vet_website.vet_website.doctype.vetcustomerinvoice.vetcustomerinvoice.get_mutasi_piutang",
+                    args: { filters: JSON.parse(sessionStorage.getItem(window.location.pathname)), mode: td.state.mode, all: 1 },
+                    callback: function (r) {
+                        if (r.message) {
+                            console.log(r.message)
+                            td.setState({ 'print_data': r.message.data, 'loaded': true, 'datalength': r.message.datalength });
+                            setTimeout(function() {
+                                td.printPDF()
+                            }, 3000);
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                frappe.msgprint(('Month or Year must be selected'));
+            }
         }
     }
 
     printPDF() {
-        var title = 'MutasiPiutang'
+        var title = 'MutasiPiutang-'
+        var filters = JSON.parse(sessionStorage.getItem(window.location.pathname))
+
+        if (filters.invoice_date != undefined && this.state.mode != undefined) {
+            if (this.state.mode == 'monthly') {
+                var bulan = moment(this.state.year + '-' + this.state.month, 'YYYY-MM').format('MM-YYYY')
+                console.log(bulan)
+                title += 'Monthly-' + bulan
+            } else if (this.state.mode == 'annual') {
+                title += 'Annual-' + moment(filters.invoice_date).format('YYYY')
+            } else if (this.state.mode == 'period') {
+                var sampai_bulan = moment(this.state.year + '-' + this.state.month, 'YYYY-MM').format('MM-YYYY')
+                title += 'Periode-' + sampai_bulan
+            }
+        }
 
         // var pdfid = 'pdf'
         // var source = document.getElementById(pdfid)
@@ -113,6 +209,18 @@ class MutasiPiutang extends React.Component {
 
         var row_style2 = { 'background': '#FFFFFF', 'boxShadow': '0px 4px 23px rgba(0, 0, 0, 0.1)', 'padding': '20px 32px 20px 12px', 'marginBottom': '18px', 'height': '72px' }
         var formStyle = { border: '1px solid #397DA6', color: '#397DA6' }
+        var month_options = [<option className="d-none" key="99999"></option>]
+        var year_options = [<option className="d-none" key="99999"></option>]
+
+        var i
+        for (i = 0; i <= 11; i++) {
+            var moment_month = moment(i + 1, 'M')
+            month_options.push(<option key={moment_month.format('MM')} value={moment_month.format('MM')}>{moment_month.format('MMMM')}</option>)
+        }
+
+        this.state.list_year.forEach(function(e, index) {
+            year_options.push(<option key={e}>{e}</option>)
+        })
 
         var item_pdf = []
 
@@ -128,7 +236,7 @@ class MutasiPiutang extends React.Component {
                 if (i == 0) {
                     console.log('masuk pdf page pertama')
                     item_pdf.push(
-                        <PDF data={chunk[i]}/>
+                        <PDF data={chunk[i]} mode={this.state.mode} month={this.state.month} year={this.state.year}/>
                     )
                 } else {
                     item_pdf.push(
@@ -139,6 +247,23 @@ class MutasiPiutang extends React.Component {
         }
 
         if (this.state.loaded) {
+            var month_select, sd_period
+
+            if (this.state.mode == 'monthly' || this.state.mode == 'period') {
+
+                if (this.state.mode == 'period') {
+                    sd_period = <div className="col-auto my-auto mx-auto">
+                        s/d
+                    </div>
+                }
+
+                month_select = <div className="col-2 my-auto">
+                    <select name="month" placeholder="Month" className="form-control" value={this.state.month} onChange={e => this.filterChange(e)}>
+                        {month_options}
+                    </select>
+                </div>
+            }
+
             return (
                 <div>
                     <div className="row mx-0" style={row_style2}>
@@ -147,6 +272,24 @@ class MutasiPiutang extends React.Component {
                         </div>
                         <div className="col-2 my-auto">
                             <input value={this.state.search || ''} className="form-control fs12" name="search" placeholder="Search..." style={formStyle} onChange={e => this.setState({ search: e.target.value })} onKeyDown={(e) => e.key === 'Enter' ? this.setFilter(JSON.parse(sessionStorage.getItem(window.location.pathname))) : null} />
+                        </div>
+                        <div className="col-2 my-auto">
+                            <select name="mode" placeholder="Periode" className="form-control" value={this.state.mode} onChange={e => this.setMode(e)}>
+                                <option className="d-none" key="99999"></option>
+                                <option value="monthly">Monthly</option>
+                                <option value="annual">Annual</option>
+                                <option value="period">Period</option>
+                            </select>
+                        </div>
+                        {sd_period}
+                        {month_select}
+                        <div className="col-2 my-auto">
+                            <select name="year" placeholder="Year" className="form-control" value={this.state.year} onChange={e => this.filterChange(e)}>
+                                {year_options}
+                            </select>
+                        </div>
+                        <div className="col-2 my-auto">
+                            <button type="button" className="btn btn-outline-danger text-uppercase fs12 fwbold" onClick={() => this.setFilter()}>Set</button>
                         </div>
                     </div>
                     <MutasiPiutangList items={this.state.data} paginationClick={this.paginationClick} currentpage={this.state.currentpage} datalength={this.state.datalength} />
@@ -300,6 +443,21 @@ class PDF extends React.Component {
         var invoice2 = { letterSpacing: 0 }
         var thead = { background: '#d9d9d9', fontSize: 11 }
         var table_rows = []
+        var subtitle = ''
+        var filters = JSON.parse(sessionStorage.getItem(window.location.pathname))
+
+        if (filters.invoice_date != undefined && this.props.mode != undefined) {
+            if (this.props.mode == 'monthly') {
+                var bulan = moment(this.props.year + '-' + this.props.month, 'YYYY-MM').format('MM-YYYY')
+                console.log(bulan)
+                subtitle = 'Monthly ' + bulan
+            } else if (this.props.mode == 'annual') {
+                subtitle = 'Annual ' + moment(filters.invoice_date).format('YYYY')
+            } else if (this.props.mode == 'period') {
+                var sampai_bulan = moment(this.props.year + '-' + this.props.month, 'YYYY-MM').format('MM-YYYY')
+                subtitle = 'Periode ' + sampai_bulan
+            }
+        }
 
         data.forEach((d, index) => {
             table_rows.push(
@@ -337,7 +495,7 @@ class PDF extends React.Component {
                             </div>
                             <div className="col-4 px-0">
                                 <p className="fwbold text-right text-uppercase fs28" style={invoice}>Mutasi Piutang</p>
-                                <p className="fw600 text-right text-uppercase fs14" style={invoice2}></p>
+                                <p className="fw600 text-right text-uppercase fs14" style={invoice2}>{subtitle}</p>
                             </div>
                             <div className="col-12" style={borderStyle} />
                         </div>
