ffa2df97ae649ab6dfd11993759089a2f1bb4628
rezazrna <rezazesariana58@gmail.com>
2023-07-05 18:31:56 +0700

    laporan rabies 3

--- vet_website/vet_website/doctype/vetrekammedis/vetrekammedis.py
+++ vet_website/vet_website/doctype/vetrekammedis/vetrekammedis.py
@@ -217,7 +217,7 @@ def get_rekam_medis_form(name):
 @frappe.whitelist()
 def get_rabies(filters=None, mode=False, all=False):
 	invoice_filters = {'is_refund': False}
-	line_or_filters = {'product_name': ['like', '%Defensor/%'], 'product_name': ['like', '%Rabisin/%']}
+	line_or_filters = {'product_name': ['like', '%Rabies%'], 'product_name': ['like', '%Rabies%']}
 	line_filters = {}
 
 	filter_json = False
@@ -242,7 +242,7 @@ def get_rabies(filters=None, mode=False, all=False):
 
 		if invoice_date:
 			if mode == 'monthly' or mode == 'period':
-				max_date_dt = datetime.strptime(invoice_date, '%Y-%m-%d') - relativedelta.rd(days=1)
+				max_date_dt = datetime.strptime(invoice_date, '%Y-%m-%d') - relativedelta(days=1)
 			else:
 				max_date_dt = datetime.strptime(invoice_date, '%Y-%m-%d')
 
